e26732902ba340464563db488079b817a685db5d
somang <y1810@naverl.com>
2025-06-24 13:03:48 +0900

    change10

--- config/db.js
+++ config/db.js
@@ -2,12 +2,20 @@ import { Pool } from 'pg';
 
 // PostgreSQL 연결 설정
 const pool = new Pool({
+  user: 'ksomang', 
+  host: 'localhost',
+  database: 'sprint_mission3',
+  password: '1810',
+  port: 5432,               
+});
+
+/* const pool = new Pool({
   user: 'sprint_mission3_nvsd_user', 
   host: 'dpg-d1cf6vgdl3ps73fj8glg-a',
   database: 'sprint_mission3_nvsd',
   password: '8ZusvsSToJ3kMwSYbyqji41xDUfRBVuG',
   port: 5432,               
-});
+}); */
 
 // 연결 테스트
 pool.connect((err, client, release) => {
